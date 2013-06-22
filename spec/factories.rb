5892d4900ca238662bf4c3fa04d9756e5ffaab83
Neal Gupta <nealgupta@hotmail.com>
2013-06-21 23:16:29 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,23 @@
+# FactoryGirl.define do
+#   factory :user do
+#     name     "Neal Gupta"
+#     email    "nealgupta@hotmail.com"
+#     password "foobar"
+#     password_confirmation "foobar"
+#   end
+# end
+
+#define a Factory Girl sequence
 FactoryGirl.define do
   factory :user do
-    name     "Neal Gupta"
-    email    "nealgupta@hotmail.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    # adding a factory for admin users
+    factory :admin do
+      admin true
+    end
   end
 end
